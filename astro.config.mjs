e0c590c46ea3529e8666f0a31e5e5336ebcb8019
01-donkey <3191joseph@gmail.com>
2026-04-13 21:47:37 +0800

    用好線性回歸

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,22 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import mdx from '@astrojs/mdx';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
+  // 1. 這裡保留您原本 Tailwind v4 的 Vite 設定
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [mdx()]
+  // 2. 這裡放入 MDX 以及數學引擎的外掛
+  integrations: [
+    mdx({
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [rehypeKatex],
+    })
+  ]
 });
---
+++ src/pages/projects/random-forest.md
@@ -0,0 +1,8 @@
+---
+title: "隨機森林"
+date: "2026-04-013"
+description: ""
+layout: ../../layouts/Layout.astro
+---
+
+## 隨機森林
